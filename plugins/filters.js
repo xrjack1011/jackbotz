1908a80e0dc780d70f657b8039030f4031ee3484
BADBOY MODER <88477572+BADBMODER@users.noreply.github.com>
2021-09-29 11:49:04 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -52,7 +52,7 @@ Asena.addCommand({on: 'text', fromMe: false }, (async (message, match) => {
     if(Config.BGMFILTER){
         let banned = jid.find( Jid => Jid === message.jid);
         if(banned !== undefined) return
-        if (!!message.mention && message.mention[0] == '919072790587@s.whatsapp.net') {
+        if (!!message.mention && message.mention[0] == '918593079460@s.whatsapp.net') {
 await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/mention.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted : message.data, ptt: true})
         }
 const array = ['acho','Velluveli','udayp','tutu','Thug','suhurth','Single boy','Settak','sena','Sed','sed life','sed bgm','Sarasu','Role','Reyali','Poyalo','power varate','Power an','Pottikum','poli','Podamyr','pm vada','Pabam','onu poda chekka','oh','O','number pottikum','no love','nerth','Nenak nan und','ne power an','ne etha','nanban fake','nan und','MPL','Monuse','money hist bgm','mone','Mandan','Machane','lshta','Love','Love you','Love me','line','line undo','Ketto','karale','Kane','kalikuno','kalikkan nekkale','Kalikkale','joker','Inna pedicho','i love u','I love you baby','@918593079460','aliya','ano','arakkal tharavad','arakkal','Aran ariyo','ariyo','arulla','ayn','Ayo','AyoAyo','Badboy','Bgm ettutharam','Bgm ille','BGM poratte','Bgm','Da','Hi','hlo','kannapi','kollamo','kollum nan','myr','ok','polichille','poda','ramana','sad','sorry','tharilla','Boss ethi','Bosse','chalakallada','Chank uyr','Chank','chankan','Chettan oru bgm ettu tharate','coming','Don','Dream','gays','ennalum','Entry','ennit','Group','Groupel']
@@ -118,7 +118,7 @@ Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
         if(Config.BGMFILTER){
         let banned = jid.find( Jid => Jid === message.jid);
         if(banned !== undefined) return
-        if (!!message.mention && message.mention[0] == '919072790587@s.whatsapp.net') {
+        if (!!message.mention && message.mention[0] == '918593079460@s.whatsapp.net') {
 await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/trance.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted : message.data, ptt: true})
         }
         if (!!message.mention && message.mention[0] == Config.AFNN) {
