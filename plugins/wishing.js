e5706bfb4040cdb5ca07c282d4d66e7951d063e8
BADBOY MODER <88477572+BADBMODER@users.noreply.github.com>
2021-10-17 21:40:59 +0530

    Update wishing.js

--- plugins/wishing.js
+++ plugins/wishing.js
@@ -7,7 +7,7 @@ const Config = require('../config');
 
 
 if (Config.WORKTYPE == 'private') {
-asena.addCommand({pattern: 'gdmrng', fromMe: true, deleteCommand: true, desc: GM,}, (async (message, match) => {
+asena.addCommand({pattern: 'gm', fromMe: true, deleteCommand: true, desc: GM,}, (async (message, match) => {
 
     var r_text = new Array ();
 
@@ -28,7 +28,7 @@ asena.addCommand({pattern: 'gdmrng', fromMe: true, deleteCommand: true, desc: GM
     }));
 
 
-asena.addCommand({pattern: 'gdni8', fromMe: true, deleteCommand: true, desc: GN,}, (async (message, match) => {
+asena.addCommand({pattern: 'gn', fromMe: true, deleteCommand: true, desc: GN,}, (async (message, match) => {
 
         var r_text = new Array ();
     
@@ -51,7 +51,7 @@ asena.addCommand({pattern: 'gdni8', fromMe: true, deleteCommand: true, desc: GN,
     
 
     if (Config.WORKTYPE == 'public') {
-        asena.addCommand({pattern: 'gdmrng', fromMe: false, deleteCommand: true, desc: GM,}, (async (message, match) => {
+        asena.addCommand({pattern: 'gm', fromMe: false, deleteCommand: true, desc: GM,}, (async (message, match) => {
         
             var r_text = new Array ();
         
@@ -72,7 +72,7 @@ asena.addCommand({pattern: 'gdni8', fromMe: true, deleteCommand: true, desc: GN,
             }));
         
         
-        asena.addCommand({pattern: 'gdni8', fromMe: false, deleteCommand: true, desc: GN,}, (async (message, match) => {
+        asena.addCommand({pattern: 'gn', fromMe: false, deleteCommand: true, desc: GN,}, (async (message, match) => {
         
                 var r_text = new Array ();
             
@@ -90,7 +90,7 @@ asena.addCommand({pattern: 'gdni8', fromMe: true, deleteCommand: true, desc: GN,
                     message.jid,(r_text[i]), MessageType.text);
             
                 }));    
-                asena.addCommand({pattern: 'gdmrng', fromMe: true, deleteCommand: true,dontAddCommandList: true }, (async (message, match) => {
+                asena.addCommand({pattern: 'gm', fromMe: true, deleteCommand: true,dontAddCommandList: true }, (async (message, match) => {
         
                     var r_text = new Array ();
                 
@@ -111,7 +111,7 @@ asena.addCommand({pattern: 'gdni8', fromMe: true, deleteCommand: true, desc: GN,
                     }));
                 
                 
-                asena.addCommand({pattern: 'gdni8', fromMe: true, deleteCommand: true,dontAddCommandList: true }, (async (message, match) => {
+                asena.addCommand({pattern: 'gn', fromMe: true, deleteCommand: true,dontAddCommandList: true }, (async (message, match) => {
                 
                         var r_text = new Array ();
                     
