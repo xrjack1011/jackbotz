6e406da29ccd23a1db0d116ed45bfe4f28d0f9ee
BADBOY MODER <88477572+BADBMODER@users.noreply.github.com>
2021-10-19 19:57:21 +0530

    Update report.js

--- plugins/report.js
+++ plugins/report.js
@@ -3,7 +3,7 @@ Telegram: t.me/phaticusthiccy
 Instagram: www.instagram.com/kyrie.baran
 */
 
-const Asena = require('../events');
+const Badboy = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const Config = require('../config');
 
@@ -11,7 +11,7 @@ const Language = require('../language');
 const Lang = Language.getString('tagall');
 
 if (Config.WORKTYPE == 'private') {
-    Asena.addCommand({pattern: 'report ?(.*)', fromMe: true, desc: Lang.REPORT}, (async (message, match) => {
+    Badboy.addCommand({pattern: 'report ?(.*)', fromMe: true, desc: Lang.REPORT}, (async (message, match) => {
         if (match[1] == '' && message.reply_message) {
             let grup = await message.client.groupMetadata(message.jid);
             var jids = [];
@@ -43,37 +43,7 @@ if (Config.WORKTYPE == 'private') {
     }));
 }
 else if (Config.WORKTYPE == 'public') {
-    Asena.addCommand({pattern: 'report ?(.*)', fromMe: false, desc: Lang.REPORT}, (async (message, match) => {
-        if (match[1] == '' && message.reply_message) {
-            let grup = await message.client.groupMetadata(message.jid);
-            var jids = [];
-            mesaj = '';
-            grup['participants'].map(async (uye) => {
-                if (uye.isAdmin) {
-                    mesaj += '@' + uye.id.split('@')[0] + ' ';
-                    jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
-                }
-            });
-            await message.client.sendMessage(message.jid,Lang.USER + '@' + message.reply_message.jid.split('@')[0] , MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
-        
-        }
-        else if (match[1] !== '' && message.reply_message) {
-            let grup = await message.client.groupMetadata(message.jid);
-            var jids = [];
-            mesaj = '';
-            grup['participants'].map(async (uye) => {
-                if (uye.isAdmin) {
-                    mesaj += '@' + uye.id.split('@')[0] + ' ';
-                    jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
-                }
-            });
-            await message.client.sendMessage(message.jid,Lang.USER + '@' + message.reply_message.jid.split('@')[0] + Lang.REASON + `${match[1]}`, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
-        }
-        else if (!message.reply_message) {
-            return message.client.sendMessage(message.jid,Lang.REPLY, MessageType.text);
-        }
-    }));
-    Asena.addCommand({pattern: 'report ?(.*)', fromMe: true, desc: Lang.REPORT, dontAddCommandList: true}, (async (message, match) => {
+    Badboy.addCommand({pattern: 'report ?(.*)', fromMe: false, desc: Lang.REPORT}, (async (message, match) => {
         if (match[1] == '' && message.reply_message) {
             let grup = await message.client.groupMetadata(message.jid);
             var jids = [];
