365261036c9f0bf2fe84bb0eb3928b0caa85ea71
BADBOY MODER <88477572+BADBMODER@users.noreply.github.com>
2021-10-19 20:54:12 +0530

    Update gsarena.js

--- plugins/gsarena.js
+++ plugins/gsarena.js
@@ -1,7 +1,9 @@
+
 /* created by afnanplk
+re edited by BADBMODER
 */
 
-const Asena = require('../events');
+const Badboy = require('../events');
 const { MessageType } = require('@adiwajshing/baileys');
 const got = require('got');
 const Config = require('../config');
@@ -13,7 +15,7 @@ const Lang = Language.getString('weather');
 
 if (Config.WORKTYPE == 'private') {
 	
-Asena.addCommand({pattern: 'device ?(.*)', fromMe: true, desc: Lang.DEVICE , dontAddCommandList: true }, async (message, match) => {
+Badboy.addCommand({pattern: 'device ?(.*)', fromMe: true, desc: Lang.DEVICE , dontAddCommandList: true }, async (message, match) => {
 	const url = `https://docs-jojo.herokuapp.com/api/gsm_arena?q=${match[1]}`;
 	try {
 		const response = await got(url);
@@ -29,7 +31,7 @@ Asena.addCommand({pattern: 'device ?(.*)', fromMe: true, desc: Lang.DEVICE , don
 
 else if (Config.WORKTYPE == 'public') {
 	
-Asena.addCommand({pattern: 'device ?(.*)', fromMe: false, desc: Lang.DEVICE , dontAddCommandList: true }, async (message, match) => {
+Badboy.addCommand({pattern: 'device ?(.*)', fromMe: false, desc: Lang.DEVICE , dontAddCommandList: true }, async (message, match) => {
 	const url = `https://docs-jojo.herokuapp.com/api/gsm_arena?q=${match[1]}`;
 	try {
 		const response = await got(url);
@@ -40,10 +42,6 @@ Asena.addCommand({pattern: 'device ?(.*)', fromMe: false, desc: Lang.DEVICE , do
 	} catch {
 		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
 	}
-});
-	 Asena.addCommand({pattern: 'what now', fromMe: false, desc: Lang.NEWEP }, (async (message, match) => {  
-       
-        await message.sendMessage('*CMND* \n .device\n *DESC* \n know details of a mobilephone\n *example:* .device rog 5\n\n *CMND* \n .rdmore\n *DESC* \n add readmore after give text\n *example:* .rdmore pinky\n\n *CMND* \n.brdmore\n *DESC* \n add readmore before given text\n *example:* .brdmore pinky\n\n *CMND* \n.bgm one \n .bgm two \n *DESC* \n will change the type of reply bgm \n *example:* .bgm one (.bgm one \n is the default bgm mode - to change that use .bgm two ) ', MessageType.text,{ quoted: message.data });
-        
-    }));
+});	
+
 }
