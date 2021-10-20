a84a2f73f737c86d8ce6a99a04d83c24b838ca3b
BADBOY MODER <88477572+BADBMODER@users.noreply.github.com>
2021-10-20 08:29:35 +0530

    Update and rename badboy.js to pinky.js

--- pinky.js
+++ pinky.js
@@ -1,5 +1,4 @@
 /* codded by afnanplk
-re coded by BADBMODER
 */
 
 const { Sequelize } = require('sequelize');
@@ -12,12 +11,9 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
- OB_NAME: process.env.Z_BOT_NAME === undefined ? 'badboy' : process.env.Z_BOT_NAME,
- OA_NAME: process.env.Z_DEP_NAME === undefined ? 'badboy' : process.env.Z_DEP_NAME,
  REMOVE2: process.env.THERI_KICK_PM === undefined ? 'false' : process.env.THERI_KICK_PM,
  PLKAFN: process.env.THERI_LIST_PM === undefined ? false : process.env.THERI_LIST_PM,
- OA_REPLY: process.env.Z_DEP_REPLY === undefined ? 'you typed badboy , he is my creator' : process.env.Z_DEP_REPLY,
- PHONE: process.env.NUMBER === undefined ? '+919645697828' : process.env.NUMBER,   
-    
+ PHONE: process.env.NUMBER === undefined ? '+918301985859' : process.env.NUMBER,   
+ OA_NAME: process.env.DEPLOYER === undefined ? 'Someone' : process.env.DEPLOYER,    
 
 };
